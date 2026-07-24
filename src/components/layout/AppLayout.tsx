756ef00745f4aa7e0ab500d0d4baa67ceec09ffb
Swapnil-kapile.dipakrao <117730243+swapnil123kapile@users.noreply.github.com>
2026-07-24 11:57:45 +0530

    Add layered ambient background

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -8,13 +8,22 @@ interface AppLayoutProps {
 
 export function AppLayout({ onGenerate }: AppLayoutProps) {
   return (
-    <div className="flex h-screen w-full overflow-hidden bg-background">
-      <Sidebar />
-      <div className="flex h-full min-w-0 flex-1 flex-col">
-        <TopNav onGenerate={onGenerate} />
-        <main className="min-h-0 flex-1 overflow-hidden">
-          <Outlet />
-        </main>
+    <div className="relative flex h-screen w-full overflow-hidden bg-background bg-vignette">
+      {/* Ambient background layer */}
+      <div className="bg-ambient">
+        <div className="bg-orb bg-orb-primary" />
+        <div className="bg-orb bg-orb-secondary" />
+        <div className="bg-orb bg-orb-accent" />
+      </div>
+
+      <div className="relative z-10 flex h-full w-full min-w-0">
+        <Sidebar />
+        <div className="flex h-full min-w-0 flex-1 flex-col">
+          <TopNav onGenerate={onGenerate} />
+          <main className="min-h-0 flex-1 overflow-hidden">
+            <Outlet />
+          </main>
+        </div>
       </div>
     </div>
   );
